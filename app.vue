b47901bf37fa91392b92601eab863932ea5929f8
ministrov <61511030+ministrov@users.noreply.github.com>
2023-02-13 19:57:34 +0300

    feat: add Logo component and file composable

--- app.vue
+++ app.vue
@@ -1,20 +1,24 @@
 <template>
-  <div :class="{'dark': darkMode}">
+  <div :class="{ 'dark': darkMode }">
 
-    <div class="min-h-full">
+    <div class="bg-white dark:bg-dim-900">
 
-      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+      <div class="min-h-full">
 
-        <!-- Left sidebar -->
-        <div class="hidden bg-red-500 md:block xs-col-span-1 xl:col-span-2">
-          <div class="sticky top-0">
-            <SidebarLeft/>
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+
+          <!-- Left sidebar -->
+          <div class="md:block xs-col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
           </div>
+
+          <!-- Main content -->
+
+          <!-- Right sidebar -->
+
         </div>
-
-        <!-- Main content -->
-
-        <!-- Right sidebar -->
 
       </div>
 
@@ -24,7 +28,7 @@
 </template>
 
 <script setup>
-
+let darkMode = ref(false);
 </script>
 
 <style lang="scss" scoped>
--- components/Sidebar/Left.vue
+++ components/Sidebar/Left.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
-    Left sidebar
+  <div class="flex flex-col h-screen w-min">
+
+    <div class="p-2 my-2 hover:bg-red-300 rounded-full w-min dark:hover:bg-white/20" :class="defaultTransition">
+      <NuxtLink to="/">
+        <div class="w-8 h-8">
+          <LogoTwitter />
+        </div>
+      </NuxtLink>
+    </div>
+
   </div>
 </template>
 
 <script setup>
+const {defaultTransition} = useTailwindConfig();
 
 </script>
 
