9f99a400253c88c43a1dab10aaeedd2d7b8f0376
ministrov <61511030+ministrov@users.noreply.github.com>
2023-02-13 18:34:29 +0300

    feat: install tailwind and connect it  into project. And also create file components and firs component

--- app.vue
+++ app.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div :class="{'dark': darkMode}">
+
+    <div class="min-h-full">
+
+      <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+
+        <!-- Left sidebar -->
+        <div class="hidden bg-red-500 md:block xs-col-span-1 xl:col-span-2">
+          <div class="sticky top-0">
+            <SidebarLeft/>
+          </div>
+        </div>
+
+        <!-- Main content -->
+
+        <!-- Right sidebar -->
+
+      </div>
+
+    </div>
+
   </div>
 </template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ components/Sidebar/Left.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Left sidebar
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
